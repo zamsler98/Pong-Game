06b897359137c76d5d0afc0e8f0a8d490ca4df74
zamsler98 <zamsler98@gmail.com>
2020-09-12 14:16:48 -0500

    Init

---
+++ Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Ball ball;
+    public Paddle paddle;
+
+    public static Vector2 bottomLeft;
+    public static Vector2 topRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bottomLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0));
+        topRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+
+
+        Instantiate(ball);
+        var paddle1 = Instantiate(paddle) as Paddle;
+        var paddle2 = Instantiate(paddle) as Paddle;
+        paddle1.Init(true); //Right paddle
+        paddle2.Init(false); //Left paddle
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Paddle.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    float height;
+
+    string input;
+    public bool isRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        height = transform.localScale.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis(input) * Time.deltaTime * speed;
+
+        if (transform.position.y < GameManager.bottomLeft.y + height / 2 && move < 0)
+        {
+            move = 0;
+        }
+        if (transform.position.y > GameManager.topRight.y - height / 2 && move > 0)
+        {
+            move = 0;
+        }
+        transform.Translate(move * Vector2.up);
+    }
+
+    public void Init(bool isPaddleRight)
+    {
+        Vector2 pos;
+        isRight = isPaddleRight;
+        if (isPaddleRight)
+        {
+            pos = new Vector2(GameManager.topRight.x, 0);
+            pos -= Vector2.right * transform.localScale.x;
+            input = "PaddleRight";
+        }
+        else
+        {
+            pos = new Vector2(GameManager.bottomLeft.x, 0);
+            pos += Vector2.right * transform.localScale.x;
+            input = "PaddleLeft";
+        }
+
+        //Update this paddle's position
+        transform.position = pos;
+
+        transform.name = input;
+    }
+}
