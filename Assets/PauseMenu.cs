8f07651b3b7f3ae5a53d8a11309f6df1069cc28e
zamsler98 <zamsler98@gmail.com>
2020-09-15 19:48:08 -0500

    First draft of pause menu

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    public void Restart()
+    {
+        //Start the game with the same game type we currently have
+        GameManager.StartGame(GameManager.GameType);
+        Resume();
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("Main Menu");
+        Resume();
+    }
+}
